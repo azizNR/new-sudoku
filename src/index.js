c94bdc7d4bcf7dd69949d49dd7eb8e0c5ca43195
Aziz <aziz.narkhabilov@mail.ru>
2019-07-06 19:38:26 +0500

    add -A

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function solveSudoku(matrix) {
   // your solution
+    var values = [1,2,3,4,5,6,7,8,9];
+    var _sudoku = matrix;
+    for (y=0; y<9; y++) {
+        var _values = values.map(function(v){return v;});
+        for (x=0; x<9; x++) {
+            if (matrix[y][x] != 0) {
+                _values.splice(_values.indexOf(matrix[y][x]), 1);
+            }
+        }
+        for (x=0; x<9; x++) {
+            if (matrix[y][x] == 0) {
+                _sudoku[y][x] = _values.map(function(v){return v;});
+            }
+        }
+    }
+    return(matrix);
+
 }
